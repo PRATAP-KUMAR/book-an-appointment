6f095c114a70c82d672a344e7281333e8db6e39e
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2023-01-05 10:30:48 +0200

    Add-Seed-Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,55 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Car.create([
+    {
+        name: "Car",
+        image: "https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+        description: "automobile, byname auto, also called motorcar or car,
+                      a usually four-wheeled vehicle designed primarily for passenger
+                      transportation and commonly propelled by an internal-combustion
+                      engine using a volatile fuel. ",
+        model: "Chevrolet Camaro 2020",
+        manufacturing_date: 2020-04-21 
+    },
+    {
+        name: "Car",
+        image: "https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+        description: "automobile, byname auto, also called motorcar or car,
+                      a usually four-wheeled vehicle designed primarily for passenger
+                      transportation and commonly propelled by an internal-combustion
+                      engine using a volatile fuel. ",
+        model: "Chevrolet Camaro 2020",
+        manufacturing_date: 2020-04-21 
+    },
+    {
+        name: "Car",
+        image: "https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+        description: "automobile, byname auto, also called motorcar or car,
+                      a usually four-wheeled vehicle designed primarily for passenger
+                      transportation and commonly propelled by an internal-combustion
+                      engine using a volatile fuel. ",
+        model: "Chevrolet Camaro 2020",
+        manufacturing_date: 2020-04-21 
+    },
+    {
+        name: "Car",
+        image: "https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+        description: "automobile, byname auto, also called motorcar or car,
+                      a usually four-wheeled vehicle designed primarily for passenger
+                      transportation and commonly propelled by an internal-combustion
+                      engine using a volatile fuel. ",
+        model: "Chevrolet Camaro 2020",
+        manufacturing_date: 2020-04-21 
+    },
+    {
+        name: "Car",
+        image: "https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+        description: "automobile, byname auto, also called motorcar or car,
+                      a usually four-wheeled vehicle designed primarily for passenger
+                      transportation and commonly propelled by an internal-combustion
+                      engine using a volatile fuel. ",
+        model: "Chevrolet Camaro 2020",
+        manufacturing_date: 2020-04-21 
+    }
+])
