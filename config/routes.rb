f46a798c411f59dd24a5344e28206a120e4e5914
Raga <ragawilliam570@gmail.com>
2023-01-05 13:20:55 +0300

    Add devise routes API endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  }, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   resources :reservations
   resources :cars
   resources :users
