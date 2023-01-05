12a2846c340c164ec559c698b7b7d92ff8b93f4e
Raga <ragawilliam570@gmail.com>
2023-01-05 14:31:41 +0300

    Add sessions controller methods to return json

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
-  # before_action :configure_sign_in_params, only: [:create]
+  # before_action :configure_sign_in_params, only: [:create] if: :devise_controller?
 
   # GET /resource/sign_in
   # def new
@@ -19,10 +19,31 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+  
+  def respond_with(resource, _opts = {})
+    render json: {
+      status: {code: 200, message: 'Logged in successfully.'},
+      data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+      }, status: :ok
+  end
+
+  def respond_to_on_destroy
+    if current_user
+      render json: {
+        status: 200,
+        message: 'Logged out successfully'
+      }, status: :ok
+    else
+      render json: {
+        status: 401,
+        message: "Could not find active session"
+      }, status: :unauthorized
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
   # end
 end
