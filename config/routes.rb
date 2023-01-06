b1f6c8148afc22eb54870e389fba3ddcb81b5de8
Raga <ragawilliam570@gmail.com>
2023-01-06 10:29:44 +0300

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
-    sign_up: 'signup'
+    registration: 'signup'
   }, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
