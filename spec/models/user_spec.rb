061db17548bea109763ca961d075e218ca4b3d55
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2023-01-10 12:54:19 +0200

    Test not valid output

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,11 @@ RSpec.describe User, type: :model do
   it 'is valid with valid attributes' do
     expect(subject).to be_valid
   end
+
+  it 'is not valid without a name' do
+    user = User.new(username: nil)
+    expect(user).to_not be_valid
+  end
+
+
 end
