23cbe8686af86633f312b294ceb7b3b80fdd1ee1
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2023-01-10 13:13:56 +0200

    Remove commented code

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -7,18 +7,4 @@ RSpec.describe 'Users', type: :request do
       expect(response).to have_http_status(200)
     end
   end
-
-  # path '/api/v1/users' do
-  #   get('list users') do
-  #     response(200, 'successful') do
-  #       after do |example|
-  #         example.metadata[:response][:content] = {
-  #           'application/json' => {
-  #             example: JSON.parse(response.body, symbolize_names: true)
-  #           }
-  #         }
-  #       end
-  #       run_test!
-  #     end
-  #   end
 end
