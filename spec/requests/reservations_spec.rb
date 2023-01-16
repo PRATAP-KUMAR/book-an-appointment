38390010610c2713a1872cc0ed85278696fc7e6e
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2023-01-16 10:08:18 +0200

    update reservations spec

--- spec/requests/reservations_spec.rb
+++ spec/requests/reservations_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Reservations', type: :request do
-  describe 'GET /reservations' do
+  subject do
+    %w[user admin].each do |role|
+      Role.find_or_create_by({ name: role })
+    end
+    user = User.create(username: 'User', email: 'Email@example.com', password: 'Password', password_confirmation: "password", role: Role.find_by_name('admin'))
+  end
+
     it 'Get reservations' do
-      get reservations_path
+      get user_reservations_path(1)
       expect(response).to have_http_status(200)
     end
-  end
 end
