5db9522ab69eea6d299c759b602ee8ac131d1443
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2023-01-16 10:12:58 +0200

    Fixed linters

--- spec/requests/reservations_spec.rb
+++ spec/requests/reservations_spec.rb
@@ -5,11 +5,12 @@ RSpec.describe 'Reservations', type: :request do
     %w[user admin].each do |role|
       Role.find_or_create_by({ name: role })
     end
-    user = User.create(username: 'User', email: 'Email@example.com', password: 'Password', password_confirmation: "password", role: Role.find_by_name('admin'))
+    User.create(username: 'User', email: 'Email@example.com', password: 'Password',
+                       password_confirmation: 'password', role: Role.find_by_name('admin'))
   end
 
-    it 'Get reservations' do
-      get user_reservations_path(1)
-      expect(response).to have_http_status(200)
-    end
+  it 'Get reservations' do
+    get user_reservations_path(1)
+    expect(response).to have_http_status(200)
+  end
 end
