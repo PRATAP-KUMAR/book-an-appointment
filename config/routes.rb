bcc981a945600a3eb28d062ec92748f8d7111b89
Raga <ragawilliam570@gmail.com>
2023-01-20 11:31:44 +0300

    Add destroy path

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
-  resources :cars, only: [:index, :show, :create]
+  resources :cars, only: [:index, :show, :create, :destroy]
   resources :users do
     resources :reservations, only: [:index, :create]
   end
